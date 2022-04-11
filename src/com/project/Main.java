b0df77bdba987fb249bd8b28913083c4a059fa80
david riyan kurniawan <103322177+davidriyan@users.noreply.github.com>
2022-04-11 22:37:02 +0700

    Add files via upload
    ini merupakan perulangan while-java sederhana.

---
+++ src/com/project/Main.java
@@ -0,0 +1,12 @@
+package com.project;
+public class Main{
+
+    public static void main(String[]args) {
+        //perulangan while sederhana
+       int a = 0;
+       while(a <= 10){
+           System.out.println(a);
+           a = a+2;
+       }
+    }
+}
